415432689517bf96a2f9fca605273d25cf10539e
anirba-ux <anirbanaddastudent@gmail.com>
2024-11-26 21:12:05 +0530

    Add files via upload
    initial commit

---
+++ src/Pages/ConsultAstrologer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ConsultAstroHero from '../components/ConsultAstroHero'
+import AstroCard from '../components/AstroCard'
+
+const ConsultAstrologer = () => {
+  return (
+    <>
+      <ConsultAstroHero />
+      <AstroCard />
+      
+    </>
+  )
+}
+
+export default ConsultAstrologer
